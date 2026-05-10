59afbd70140155c2cb2f9fcd63fda27cb3563c99
tsanders <tsanders@redhat.com>
2026-05-09 21:33:47 -0400

    Add database migration for Azure platform support

---
+++ internal/store/migrations/00047_add_azure_platform_support.sql
@@ -0,0 +1,18 @@
+-- 00047_add_azure_platform_support.sql
+-- Add Azure platform support to clusters table constraint
+
+-- +goose Up
+-- Drop the old constraint that only allowed 'aws', 'ibmcloud', 'gcp'
+ALTER TABLE clusters DROP CONSTRAINT IF EXISTS clusters_platform_check;
+
+-- Add new constraint that includes 'azure'
+ALTER TABLE clusters ADD CONSTRAINT clusters_platform_check
+  CHECK (platform IN ('aws', 'ibmcloud', 'gcp', 'azure'));
+
+-- Note: cluster_type constraint doesn't exist, so 'aro' and 'aks' types are already supported
+
+-- +goose Down
+-- Revert to previous constraint (only for rollback scenarios)
+ALTER TABLE clusters DROP CONSTRAINT IF EXISTS clusters_platform_check;
+ALTER TABLE clusters ADD CONSTRAINT clusters_platform_check
+  CHECK (platform IN ('aws', 'ibmcloud', 'gcp'));
